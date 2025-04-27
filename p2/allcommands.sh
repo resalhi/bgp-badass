e1411a8e8352c49750e64977f82995b53f844839
resalhi <ridaessalhi.70@gmail.com>
2025-04-28 00:25:49 +0100

    updates

--- p2/allcommands.sh
+++ p2/allcommands.sh
@@ -1,22 +1,60 @@
+ip adde show eth0 #(optional)
 #router 1:
 
+ip addr add 10.1.1.1/24 dev eth0
+ip link add name vxlan10 type vxlan id 10 dev eth0 local 10.1.1.1 remote 10.1.1.2 dstport 4789
+ip addr add 20.1.1.1/24 dev vxlan10
+ip link set dev vxlan10 up
 ip link add br0 type bridge
 ip link set dev br0 up
-ip addr add 10.1.1.1/24 dev eth0
-ip adde show eth0 #(optional)
-ip link add name vxlan10 type vxlan id 10 dev eth0 remote 10.1.1.2 local 10.1.1.1 dstport 4789
-ip addr add 20.1.1.1/24 dev vxlan10
 brctl addif br0 eth1
 brctl addif br0 vxlan10
+////////////////
+ip addr add 10.1.1.1/24 dev eth0
+ip link set dev eth0 up
+ip link add name vxlan10 type vxlan id 10 dev eth0 local 10.1.1.1 remote 10.1.1.2 dstport 4789
 ip link set dev vxlan10 up
+ip link add name br0 type bridge
+ip link set dev br0 up
+ip link set dev eth1 up
+ip link set dev eth1 master br0
+ip link set dev vxlan10 master br0
+ip addr add 20.1.1.1/24 dev br0
+sysctl -w net.ipv4.ip_forward=1
+
+
+
+
+
 
 #router 2:
 
 #all from before but few changes
-ip link add name vxlan10 type vxlan id 10 dev eth0 remote 10.1.1.1 local 10.1.1.2 dstport 4789
 ip addr add 10.1.1.2/24 dev eth0
+ip link add name vxlan10 type vxlan id 10 dev eth0 local 10.1.1.2 remote 10.1.1.1 dstport 4789
 ip addr add 20.1.1.2/24 dev vxlan10
 ip link set dev vxlan10 up
+ip link add br0 type bridge
+ip link set dev br0 up
+brctl addif br0 eth1
+brctl addif br0 vxlan10
+
+///////////////////////////
+ip addr add 10.1.1.2/24 dev eth0
+ip link set dev eth0 up
+ip link add name vxlan10 type vxlan id 10 dev eth0 local 10.1.1.2 remote 10.1.1.1 dstport 4789
+ip link set dev vxlan10 up
+ip link add name br0 type bridge
+ip link set dev br0 up
+ip link set dev eth1 up
+ip link set dev eth1 master br0
+ip link set dev vxlan10 master br0
+ip addr add 20.1.1.2/24 dev br0
+sysctl -w net.ipv4.ip_forward=1
+
+
+
+
 
 #host 1:
 
