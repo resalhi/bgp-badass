09316117b4b84042379605d160c9f19b81e070c8
resalhi <ridaessalhi.70@gmail.com>
2025-04-29 13:24:16 +0100

    all parts are working

--- p2/allcommands.sh
+++ p2/allcommands.sh
@@ -9,23 +9,6 @@ ip link add br0 type bridge
 ip link set dev br0 up
 brctl addif br0 eth1
 brctl addif br0 vxlan10
-////////////////
-ip addr add 10.1.1.1/24 dev eth0
-ip link set dev eth0 up
-ip link add name vxlan10 type vxlan id 10 dev eth0 local 10.1.1.1 remote 10.1.1.2 dstport 4789
-ip link set dev vxlan10 up
-ip link add name br0 type bridge
-ip link set dev br0 up
-ip link set dev eth1 up
-ip link set dev eth1 master br0
-ip link set dev vxlan10 master br0
-ip addr add 20.1.1.1/24 dev br0
-sysctl -w net.ipv4.ip_forward=1
-
-
-
-
-
 
 #router 2:
 
@@ -38,23 +21,6 @@ ip link add br0 type bridge
 ip link set dev br0 up
 brctl addif br0 eth1
 brctl addif br0 vxlan10
-
-///////////////////////////
-ip addr add 10.1.1.2/24 dev eth0
-ip link set dev eth0 up
-ip link add name vxlan10 type vxlan id 10 dev eth0 local 10.1.1.2 remote 10.1.1.1 dstport 4789
-ip link set dev vxlan10 up
-ip link add name br0 type bridge
-ip link set dev br0 up
-ip link set dev eth1 up
-ip link set dev eth1 master br0
-ip link set dev vxlan10 master br0
-ip addr add 20.1.1.2/24 dev br0
-sysctl -w net.ipv4.ip_forward=1
-
-
-
-
 
 #host 1:
 
